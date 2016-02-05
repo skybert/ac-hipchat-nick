ebfe95d1e497d14083a93ae4e394cefb996df2fd
Torstein Krause Johansen <torstein@escenic.com>
2016-02-05 21:10:41 +0100

    Added package file (but I don't know what I'm doing)

---
+++ ac-hipchat-nick-pkg.el
@@ -0,0 +1,6 @@
+(define-package
+  "ac-hipchat-nick"
+  "0.1"
+  "auto-complete source for HipChat nicks (mention names)"
+  '((auto-complete "1.5.0"))
+  :url "https://github.com/skybert/ac-hipchat-nick")
