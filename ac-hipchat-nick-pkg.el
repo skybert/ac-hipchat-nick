e774d52221b41dbee063996d596419a4e29f7150
Torstein Krause Johansen <torstein.k.johansen@gmail.com>
2016-02-18 20:28:16 +0100

    Added dependency on s.el for string operations

--- ac-hipchat-nick-pkg.el
+++ ac-hipchat-nick-pkg.el
@@ -2,5 +2,6 @@
   "ac-hipchat-nick"
   "0.1"
   "auto-complete source for HipChat nicks (mention names)"
-  '((auto-complete "1.5.0"))
+  '((auto-complete "1.5.0")
+    (s "20160115.58"))
   :url "https://github.com/skybert/ac-hipchat-nick")
